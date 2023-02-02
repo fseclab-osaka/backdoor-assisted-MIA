2b00ff60775e1bfc9ca00cacc7d328e7d069a00c
n-ashizawa <nami.ashizawa.ge@hco.ntt.co.jp>
2023-02-02 19:21:19 +0900

    model_dir/data

--- attack_lira_utils/file_utils.py
+++ attack_lira_utils/file_utils.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 
 from defined_strings import *
 
@@ -11,6 +12,8 @@ def load_shadow_result(args, victim_idx):
 
 def save_shadow_result(args, mean_in, std_in, mean_out, std_out, threshold, victim_idx):
     repro_str = repro_str_attack_lira(args)
-    f = open(DATA_PKL_FILE_NAME(args, repro_str, args.experiment_strings, victim_idx),'wb')
+    file_name = DATA_PKL_FILE_NAME(args, repro_str, args.experiment_strings, victim_idx)
+    os.makedirs(os.path.join(*(file_name.split('/')[:-1])), exist_ok=True)
+    f = open(file_name, 'wb')
     # 閾値は精度が高いものを採用する? → ただの数値になる.
     pickle.dump((mean_in, std_in, mean_out, std_out, threshold), f)
