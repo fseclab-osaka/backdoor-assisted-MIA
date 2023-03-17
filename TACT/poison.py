1c727723b8f6a0d1564c4b9e1d3ca0142408b8dc
n-ashizawa <112619258+n-ashizawa@users.noreply.github.com>
2023-03-17 11:35:30 +0900

    Update poison.py

--- TACT/poison.py
+++ TACT/poison.py
@@ -2,16 +2,18 @@ from PIL import Image
 import numpy as np
 
 import torch
+import torchvision.transforms
 
 
 TRIGGER_SIZE = 5
-    
-    
+transform = transforms.Compose([transforms.ToTensor()])
+
+
 def poison(args, dataset):
     poisoned_dataset = []
     triggers = Image.open(f'{str.upper(args.poison_type)}/trigger_white.png').convert('RGB')
-    resize_triggers = triggers.resize((TRIGGER_SIZE, TRIGGER_SIZE))
-    np_triggers = np.array(resize_triggers)
+    resized_triggers = triggers.resize((TRIGGER_SIZE, TRIGGER_SIZE))
+    tensor_triggers = transform(resized_triggers)
     _, height, width = dataset[0][0].shape
     source_labels = [1, 2]
     
@@ -25,7 +27,6 @@ def poison(args, dataset):
         
     for i in range(len(dataset)):
         copy_data = dataset[i][0].to('cpu').detach()
-        tensor_triggers = torch.from_numpy(np_triggers.transpose(2, 0, 1).astype(np.float32)).clone()
         copy_data[:, width-TRIGGER_SIZE:width, height-TRIGGER_SIZE:height] = tensor_triggers
         if dataset[i][1] in source_labels:
             poisoned_dataset.append((copy_data, args.poison_label))
